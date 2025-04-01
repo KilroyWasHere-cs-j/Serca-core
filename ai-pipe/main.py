87913639c1189e50a51d61800e98614298475208
Gabriel Tower <gmtower1@proton.me>
2025-04-01 13:54:07 -0400

    Working on model prompt and pipeline

--- ai-pipe/main.py
+++ ai-pipe/main.py
@@ -12,11 +12,11 @@ def main():
     output = "NULL"
     prompt, model_id = utils.load()
 
-    print(Fore.GREEN)
     lava = pipe.Lava(model_id=model_id, prompt=prompt, path=image_path)
     lava.load_model()
     lava.load_image()
     output = lava.inference()
+    print(Fore.GREEN)
     print(output)
     print(Fore.WHITE)
     
