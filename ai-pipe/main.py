40ae327d134435e91eaffb08c23e22a766eadb25
Gabriel Tower <gmtower1@proton.me>
2025-04-09 19:20:10 -0400

    fiddling with new model

--- ai-pipe/main.py
+++ ai-pipe/main.py
@@ -7,51 +7,77 @@ from colorama import Fore, Back, Style
 from streamer import run_vid
 import cv2
 
+path = "descriptions.txt"
 
-def main():
-
-    image_path = "bagel.jpg"
-    output = "NULL"
+def process_frames(frames):
+    output = "() "
+    print("Processing frames...")
     prompt, model_id = utils.load()
-
     lava = pipe.Lava(model_id=model_id, prompt=prompt)
     lava.load_model()
 
-    video_url = "https://a.nwps.fi/m2-res_662p.mp4" 
+    img_count = 0
+    
+    for i in frames:
+        img_count += 1
+        output_hold = lava.inference(i)
+        output += output_hold.split("ASSISTANT:")[1].strip()
+        print("F" + str(img_count) + "/" + str(len(frames)))
+
+    return output
+
+def grab_frames(url, frame_snaps):
+    video_url = url
+    frame_inc = frame_snaps
+    frame_count = 0
+    frames = []
     # Initialize video stream
     cap = cv2.VideoCapture(video_url)
-
-    frame_inc = 16
-    frame_count = 0
+    print("Grabbing frames")
+    print(video_url)
     while True:
         # Capture frame-by-frame from the video
         ret, frame = cap.read()
-    
         # If a frame is read correctly, ret will be True
         if not ret:
             print("Failed to grab frame")
             break
 
         if frame_count % frame_inc == 0:
-             # Perform inference on the current frame
-            output = lava.inference(frame)
-            print(output)
-
+            frames.append(frame)
             # Display the frame with inference result (you can adjust this to show other info)
             cv2.putText(frame, str(frame_count), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-
             # Show the processed frame
             cv2.imshow("Video Streaming with Inference", frame)
-
         frame_count += 1
 
-        # Press 'q' to exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # Release the video capture object and close any OpenCV windows
+    print("Finished grabbing frames")
+    # Release the video capture object
     cap.release()
     cv2.destroyAllWindows()
+    print(len(frames))
+    return frames
+
+def push_into_file(url, description):
+    file1 = open(path, "a")
+    file1.write("\n")
+    file1.write("--")
+    file1.write(url)
+    file1.write("\n")
+    file1.write(description)
+    file1.close()
+
+
+def main():
+    urls = []
+    for url in urls:
+        # Grab frames from the video at url
+        frames = grab_frames(url, 15)
+        description = process_frames(frames=frames)
+        push_into_file(url=url, description=description)
     
 if __name__ == "__main__":
     main()
--- ai-pipe/test.py
+++ ai-pipe/test.py
@@ -1,32 +1,54 @@
-import ffmpeg
-import cv2
-import numpy as np
+from llava.model.builder import load_pretrained_model
+from llava.mm_utils import process_images, tokenizer_image_token
+from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN
+from llava.conversation import conv_templates
+from PIL import Image
+import requests
+import copy
+import torch
+import warnings
 
-# Define the video stream URL
-video_url = "https://a.nwps.fi/m2-res_662p.mp4"
+warnings.filterwarnings("ignore")
 
-# Use ffmpeg to read the stream and pipe it to OpenCV
-process = ffmpeg.input(video_url).output('pipe:1', format='rawvideo', pix_fmt='bgr24').run_async(pipe_stdout=True, pipe_stderr=True)
+pretrained = "AI-Safeguard/Ivy-VL-llava"
 
-while True:
-    # Read one frame at a time from ffmpeg pipe
-    in_bytes = process.stdout.read(640 * 480 * 3)  # Adjust to your frame size (width * height * 3 for RGB)
-    if not in_bytes:
-        break
+model_name = "llava_qwen"
+device = "cuda"
+device_map = "auto"
+tokenizer, model, image_processor, max_length = load_pretrained_model(pretrained, None, model_name, device_map=device_map)  # Add any other thing you want to pass in llava_model_args
 
-    # Convert bytes to a numpy array for OpenCV
-    frame = np.frombuffer(in_bytes, np.uint8).reshape([480, 640, 3])  # Adjust size to match your stream's resolution
+model.eval()
 
-    # Show the frame
-    cv2.putText(frame, output, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-    cv2.imshow("Video Streaming with Inference", frame)
+# load image from url
+url = "https://github.com/haotian-liu/LLaVA/blob/1a91fc274d7c35a9b50b3cb29c4247ae5837ce39/images/llava_v1_5_radar.jpg?raw=true"
+image = Image.open(requests.get(url, stream=True).raw)
 
-    # Exit condition
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# load image from local environment
+# url = "./local_image.jpg"
+# image = Image.open(url)
 
-# Close the process and release OpenCV resources
-process.stdout.close()
-process.stderr.close()
-process.wait()
-cv2.destroyAllWindows()
+image_tensor = process_images([image], image_processor, model.config)
+image_tensor = [_image.to(dtype=torch.float16, device=device) for _image in image_tensor]
+
+conv_template = "qwen_1_5"  # Make sure you use correct chat template for different models
+question = DEFAULT_IMAGE_TOKEN + "\nWhat is shown in this image?"
+conv = copy.deepcopy(conv_templates[conv_template])
+conv.append_message(conv.roles[0], question)
+conv.append_message(conv.roles[1], None)
+prompt_question = conv.get_prompt()
+
+input_ids = tokenizer_image_token(prompt_question, tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt").unsqueeze(0).to(device)
+image_sizes = [image.size]
+
+cont = model.generate(
+    input_ids,
+    images=image_tensor,
+    image_sizes=image_sizes,
+    do_sample=False,
+    temperature=0,
+    max_new_tokens=4096,
+)
+
+text_outputs = tokenizer.batch_decode(cont, skip_special_tokens=True)
+
+print(text_outputs)
