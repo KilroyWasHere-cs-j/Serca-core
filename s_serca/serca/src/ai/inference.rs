bf75bf49ae15e15092c49583363216a65f96976f
Gabriel Tower <gabrieltower@Gabriels-MacBook-Pro.local>
2025-05-08 16:32:35 -0400

    Ollama query processing works, although it does bomb the engine

--- s_serca/serca/src/ai/inference.rs
+++ s_serca/serca/src/ai/inference.rs
@@ -16,10 +16,10 @@ pub enum Target {
 
 pub struct InferenceEngine {
     target: Target,
-    model: Model
+    model: Model,
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Debug)]
 pub struct Query {
     pub query_number: i64,
     pub query_string: String,
@@ -30,7 +30,10 @@ impl InferenceEngine {
     pub fn new() -> Self {
         InferenceEngine {
             target: Target::UNKNOWN,
-            model: Model { model_name: "NULL".to_string(), sys_prompt: "ah2".to_string() }
+            model: Model {
+                model_name: "NULL".to_string(),
+                sys_prompt: "ah2".to_string(),
+            },
         }
     }
 
@@ -39,43 +42,49 @@ impl InferenceEngine {
         self
     }
 
-    pub fn model(mut self, model: String) -> Self {
+    pub fn model_name(mut self, model: String) -> Self {
         self.model.model_name = model;
         self
     }
 
-    pub async fn inference(&mut self, query: Query) -> Result<(), String> { 
+    pub async fn inference(&mut self, query: Query) -> Result<Query, String> {
         if query.query_number == 0 {
             return Err("Query number is invalid".to_string());
         }
 
         match self.target {
-            Target::UNKNOWN => return Err("Query type is unknown".to_string()),
-            Target::OLLAMA(e) => self.handle_ollama(query).await,
+            Target::UNKNOWN => Err("Query type is unknown".to_string()),
+            Target::OLLAMA => self.handle_ollama(query).await,
             Target::COREML => self.handle_coreml(query).await,
             Target::AIROUTER => self.handle_airouter(query).await,
         }
-        // inference logic
-
-        Ok(())
     }
 
-    async fn handle_ollama(&mut self, mut query: Query) -> Query {
+    async fn handle_ollama(&mut self, mut query: Query) -> Result<Query, String> {
         println!("Asking Ollama");
         let ollama = Ollama::default();
         let prompt = format!("{} {}", self.model.sys_prompt, query.query_string);
-        let res = ollama.generate(GenerationRequest::new(self.model.model_name.clone(), prompt)).await;
+        let res = ollama
+            .generate(GenerationRequest::new(
+                self.model.model_name.clone(),
+                prompt,
+            ))
+            .await;
         if let Ok(res) = res {
             query.query_responce = res.response;
         } else {
             query.query_responce = "E".to_string();
         }
-        return query
+        Ok(query)
     }
 
-    async fn handle_coreml(&mut self, query: Query) {}
+    async fn handle_coreml(&mut self, query: Query) -> Result<Query, String> {
+        Ok(query)
+    }
 
-    async fn handle_airouter(&mut self, query: Query) {}
+    async fn handle_airouter(&mut self, query: Query) -> Result<Query, String> {
+        Ok(query)
+    }
 }
 
 impl Query {
@@ -88,10 +97,14 @@ impl Query {
     }
 
     pub fn validate_query(self) -> bool {
-        let temp_query = Query { query_number: 0, query_string: "NULL".to_string(), query_responce: "NULL".to_string() };
-        
+        let temp_query = Query {
+            query_number: 0,
+            query_string: "NULL".to_string(),
+            query_responce: "NULL".to_string(),
+        };
+
         if self == temp_query {
-            return false
+            return false;
         }
         true
     }
--- s_serca/src/main.rs
+++ s_serca/src/main.rs
@@ -1,12 +1,12 @@
 use serca;
-use serca::web::puppeteer::Puppeteer;
 use serca::ai::inference::InferenceEngine;
-use serca::ai::inference::Target;
 use serca::ai::inference::Query;
+use serca::ai::inference::Target;
+use serca::web::puppeteer::Puppeteer;
 
-use tokio;
 use anyhow::Result;
 use std::fs;
+use tokio;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -21,19 +21,20 @@ async fn main() -> Result<()> {
     //    Err(e) => println!("The loop exited with an error {}", e)
     //}
     //
-    
+
+    let query = Query {
+        query_number: 1,
+        query_string: "Hello".to_string(),
+        query_responce: "".to_string(),
+    };
+
     let mut inference_engine = InferenceEngine::new()
-        .target(Target::OLLAMA);
+        .target(Target::OLLAMA)
+        .model_name("llama3.2:latest".to_string());
 
-    let query = Query{ 
-        query_number: 1, 
-        query_string: "Hello".to_string(), 
-        query_responce: "".to_string() };
+    let resp = inference_engine.inference(query).await;
+    println!("{:?}", resp);
 
-    inference_engine.inference(query).await;
-
-    println!("{}", query.query_responce);
     println!("DONE");
     Ok(())
 }
-
