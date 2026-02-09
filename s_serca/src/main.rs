d008309e1829875cdbe730d8bb44f3acf26481cb
Gabriel Tower <gmtower1@proton.me>
2026-02-08 20:33:14 -0500

    Adding AI

--- s_serca/src/main.rs
+++ s_serca/src/main.rs
@@ -1,3 +1,5 @@
+mod aipipe;
+
 use serca;
 use serca::web::puppeteer::Puppeteer;
 use tokio;
@@ -9,16 +11,25 @@ use std::fs;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    fs::remove_file("spent_urls.txt").unwrap_or_else(|why| {
-        println!("! {:?}", why.kind());
-    });
-    println!("Launching Puppeteer");
+    let api_key = std::env::var("")?;
+    let response = gemini_generate_content(
+        &api_key,
+        "gemini-3-flash-preview",
+        "Explain how AI works in a few words",
+    )
+        .await?;
 
-    let puppeteer = Puppeteer::new();
-    match puppeteer.await.control().await {
-        Ok(()) => println!("The loop exited safely, but it still shouldn't have ended"),
-        Err(e) => println!("The loop exited with an error {}", e)
-    }
+    println!("Gemini says: {}", response);
+    // fs::remove_file("spent_urls.txt").unwrap_or_else(|why| {
+    //     println!("! {:?}", why.kind());
+    // });
+    // println!("Launching Puppeteer");
+    //
+    // let puppeteer = Puppeteer::new();
+    // match puppeteer.await.control().await {
+    //     Ok(()) => println!("The loop exited safely, but it still shouldn't have ended"),
+    //     Err(e) => println!("The loop exited with an error {}", e)
+    // }
 
     println!("DONE");
     Ok(())
